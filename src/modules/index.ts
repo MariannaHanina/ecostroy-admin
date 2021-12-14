a13e921b81d67f199a8fba468c671b8b42a6a9d2
MKhanina <mutterdu@gmail.com>
2021-12-14 14:32:08 +0300

    Added Action Module to index

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -6,4 +6,5 @@ import './pages/pages/projectsPage/index';
 import './pages/pages/videoPage/index';
 import './pages/pages/galleriesPage/index';
 import './pages/pages/servicesPage/index';
+import './pages/pages/articlesPage/index';
 import './footer/index';
